0eac0ecda360ce60c6a08043515c804edb6e3912
Lamuel Chung (qris-uq) <uqlchun1@bun052.hpc.net.uq.edu.au>
2025-08-05 21:32:45 +1000

    Set to 10-fold CV

---
+++ S21_PGOcc_2010-2024_RCV.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+#SBATCH --job-name=S21_PGOcc_2010-2024_RCV
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=256G
+#SBATCH --time=142:00:00
+#SBATCH --qos=normal
+#SBATCH --partition=general
+#SBATCH --account=a_reside
+#SBATCH --constraint=epyc3
+#SBATCH --batch=epyc3
+#SBATCH --array=0
+#SBATCH --output=log_S21_PGOcc_2010-2024_RCV_%A_%a.out
+#SBATCH --error=log_S21_PGOcc_2010-2024_RCV_%A_%a.err
+
+----------------------------------------------------
+# Load the right module (here R 4.4.0)
+module load r/4.2.1-foss-2022a
+
+
+
+---------------------------------------------------
+# Execution
+srun Rscript scripts/S21_PGOcc_2010-2024_RCV.R $SLURM_ARRAY_TASK_ID
+
